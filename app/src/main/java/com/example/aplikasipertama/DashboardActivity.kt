ed669fc0562a2261ccc3659144a7865e598a7eb7
rudi ade <adepamungkasrudi@gmail.com>
2025-12-15 23:17:23 +0700

    edit DashboardActivity, userdao,mainactivity, menambahkan permission, dan fragment map

--- app/src/main/java/com/example/aplikasipertama/DashboardActivity.kt
+++ app/src/main/java/com/example/aplikasipertama/DashboardActivity.kt
@@ -1,25 +1,56 @@
 package com.example.aplikasipertama
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DashboardActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+    private lateinit var userDao: UserDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
 
+        // Ambil ID dari intent
+        val id = intent.getIntExtra("ID", 0)
+
+        val db = AbsensiDatabase.getDatabase(this)
+        userDao = db.userDao()
+
+        // Inisialisasi EditText
+        val editUsername = findViewById<EditText>(R.id.editUsername)
+        val editEmail = findViewById<EditText>(R.id.editEmail)
+        val editFirstName = findViewById<EditText>(R.id.editFirstName)
+        val editLastName = findViewById<EditText>(R.id.editLastName)
+
+        // Ambil data user dari database
+        lifecycleScope.launch(Dispatchers.IO) {
+            val user = userDao.getUserById(id)
+            withContext(Dispatchers.Main) {
+                if (user != null) {
+                    editUsername.setText(user.username)
+                    editEmail.setText(user.email)
+                    editFirstName.setText(user.namaDepan)
+                    editLastName.setText(user.namaBelakang)
+                }
+            }
+        }
+
+        // Logout
         val buttonLogout = findViewById<Button>(R.id.buttonLogout)
         buttonLogout.setOnClickListener {
-            val intentMainActivity = Intent (this, MainActivity::class.java)
-            startActivity ( intentMainActivity)
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
 
@@ -27,8 +58,6 @@ class DashboardActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-
         }
     }
 }
--- app/src/main/java/com/example/aplikasipertama/MainActivity.kt
+++ app/src/main/java/com/example/aplikasipertama/MainActivity.kt
@@ -9,12 +9,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userDao: UserDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val db = AbsensiDatabase.getDatabase(this)
+        userDao = db.userDao()
 
         val username = findViewById<EditText>(R.id.editTextusername)
         val password = findViewById<EditText>(R.id.editTextpassword)
@@ -22,35 +29,58 @@ class MainActivity : AppCompatActivity() {
 
         var buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
 
-        buttonSubmit.setOnClickListener{
+        buttonSubmit.setOnClickListener {
+
 
             var usernameText = username.text.toString()
             var passwordText = password.text.toString()
 
-            if (usernameText.isBlank()|| passwordText.isBlank()){
+            if (usernameText.isBlank() || passwordText.isBlank()) {
 
-            Toast.makeText(this,"Username & Password tidak boleh kosong",
-                Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this, "Username & Password tidak boleh kosong",
+                    Toast.LENGTH_LONG
+                ).show()
+                lifecycleScope.launch(Dispatchers.IO) {
+                    val user = userDao.getUserByUsernameAndPassword(usernameText, passwordText)
+                    withContext(Dispatchers.Main) {
+                        if (user != null) {
+                            Toast.makeText(
+                                this@MainActivity, "Username & Password Ditemukan",
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
 
-        }else{
-            Toast.makeText(this,"Loging sedang diproses",
-                Toast.LENGTH_LONG).show()
+                            val intentPindahDashboard =
+                                Intent(this@MainActivity, DashboardActivity::class.java)
+                            intentPindahDashboard.putExtra("ID", user.id)
+                            startActivity(intentPindahDashboard)
 
-        }
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity, "Username & Password Salah",
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                        }
+                    }
+
+                }
 
 
-        }
-        val buttonDaftar = findViewById<Button>(R.id.buttonDaftar)
-        buttonDaftar.setOnClickListener {
-            val intenPindahPendaftaran = Intent(this,PendaftaranActivity::class.java)
-            startActivity(intenPindahPendaftaran)
-            finish()
-        }
+            }
+            val buttonDaftar = findViewById<Button>(R.id.buttonDaftar)
+            buttonDaftar.setOnClickListener {
+                val intenPindahPendaftaran = Intent(this, PendaftaranActivity::class.java)
+                startActivity(intenPindahPendaftaran)
+                finish()
+            }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
-}
+    }
